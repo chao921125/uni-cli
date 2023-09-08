ab4daa8d81637cfa83bfc62b1c0c1b160339ab34
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-09-08 11:51:09 +0800

    add

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import * as Pinia from "pinia";
 import App from "./App.vue";
 import { createI18n } from "vue-i18n";
 
-// import messages from "./plugins/locale/index.ts";
-// let i18nConfig = {
-// 	locale: uni.getLocale(),
-// 	messages,
-// };
-// const i18n = createI18n(i18nConfig);
+import messages from "./plugins/locale/index.ts";
+let i18nConfig = {
+	locale: uni.getLocale(),
+	messages,
+};
+const i18n = createI18n(i18nConfig);
 
 export function createApp() {
 	const app = createSSRApp(App);
 	app.use(Pinia.createPinia());
-	// app.use(i18n);
+	app.use(i18n);
 	return {
 		app,
 		Pinia,
---
+++ src/pages/template.vue
@@ -0,0 +1,22 @@
+<template>
+    <view @click="changeClick">template {{ props.name }}</view>
+</template>
+
+<script setup name="TempScriptSetup">
+const props = defineProps({
+    name: {
+        required: false,
+        type: String,
+        default: ''
+    }
+});
+const emits = defineEmits(['change']);
+const changeClick = () => {
+    emits('change', true);
+};
+defineExpose({
+    changeClick
+});
+</script>
+
+<style scoped lang="scss"></style>
