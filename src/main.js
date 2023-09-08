2a09768c23ea32f8590d34cc91fb53c1beed5759
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-09-08 11:45:51 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createSSRApp } from "vue";
-import * as  Pinia from "pinia";
+import * as Pinia from "pinia";
 import App from "./App.vue";
 import { createI18n } from "vue-i18n";
 
--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,24 @@ import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [uni()],
-  build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: false,
-      },
-    },
-  },
-  server: {
-    port: 2223,
-    strictPort: true,
-    open: true,
-    proxy: {
-      	"/api": {
-      		target: "http://jsonplaceholder.typicode.com",
-      		changeOrigin: true,
-      		rewrite: path => path.replace(/^\/api/, ""),
-      	},
-    }
-  }
+	build: {
+		minify: "terser",
+		terserOptions: {
+			compress: {
+				drop_console: false,
+			},
+		},
+	},
+	server: {
+		port: 2223,
+		strictPort: true,
+		open: true,
+		proxy: {
+			"/api": {
+				target: "http://jsonplaceholder.typicode.com",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 });
