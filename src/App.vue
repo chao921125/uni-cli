754089d92f0ce8995c401544492a26f517e095b4
hc precog <chao921125@sina.com>
2023-09-09 22:19:23 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script>
+	import config from "@/config/index";
+
 	export default {
 		globalData: {
 			title: "CC APP",
 		},
 		// 只能在App.vue里监听应用的生命周期
 		onLaunch: function () {
+			config.loadFontFace();
 			// 设置小程序分享
 			wx.showShareMenu({
 				withShareTicket: true,
--- src/main.js
+++ src/main.js
@@ -9,11 +9,8 @@ let i18nConfig = {
 };
 const i18n = createI18n(i18nConfig);
 
-import config from "@/config/index";
-
 export function createApp() {
 	const app = createSSRApp(App);
-	config.loadFontFace();
 	app.use(Pinia.createPinia());
 	app.use(i18n);
 	return {
