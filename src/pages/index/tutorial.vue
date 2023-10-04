e50bfbe761c26dbce9df13416855f4a8cbac3639
cc <chao921125@sina.com>
2023-10-04 15:56:36 +0800

    Update tutorial.vue

--- src/pages/index/tutorial.vue
+++ src/pages/index/tutorial.vue
@@ -11,6 +11,11 @@
 
 	const listData = [
 		{
+			key: "Windows",
+			url: "https://mp.weixin.qq.com/s?__biz=MzU2MjY3MjQyMw==&mid=2247483684&idx=1&sn=3a4715ac1b673a1d2adb81a6bbd49988&chksm=fc64a76ccb132e7a1c02352852c622dedf818d9a34d6e973e7d1b7fad1f95adf586445a736b4#rd",
+			title: "Windows 数字激活 HWID2",
+		},
+		{
 			key: "office",
 			url: "https://mp.weixin.qq.com/s?__biz=MzU2MjY3MjQyMw==&mid=2247483748&idx=1&sn=d18c5033017c421165473c7786e1213c&chksm=fc64a72ccb132e3a5732828263b2dac7fce2e875411f31ec5708f3e02ead12f9a116effaf742#rd",
 			title: "白嫖 Office 正版，官网激活码",
